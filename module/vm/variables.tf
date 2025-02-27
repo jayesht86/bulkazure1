0688768ae953233897a4bee6568201d4e1abab9d
jayesht86 <37490933+jayesht86@users.noreply.github.com>
2025-02-27 19:53:20 +0530

    Update variables.tf

--- module/vm/variables.tf
+++ module/vm/variables.tf
@@ -31,4 +31,70 @@ variable "zones" {
 variable "data_disk_ids" {
     type = list(string)
     default = []
-}     
+}    
+variable "linux_vm_os_disk_encryption_type" {
+  description = "Encryption type for OS Disk"
+  type        = string
+  default     = null
+}
+
+variable "linux_vm_secure_boot_enabled" {
+  description = "Enable secure boot for Gen2 VM"
+  type        = bool
+  default     = false
+}
+
+variable "linux_vm_vtpm_enabled" {
+  description = "Enable vTPM for Gen2 VM"
+  type        = bool
+  default     = false
+}
+
+variable "linux_vm_host_encryption_enabled" {
+  description = "Enable encryption at host level"
+  type        = bool
+  default     = false
+}
+
+variable "linux_vm_custom_data" {
+  description = "Base64-encoded custom data for VM initialization"
+  type        = string
+  default     = null
+}
+
+variable "linux_vm_dedicated_host_id" {
+  description = "Dedicated Host ID for VM placement"
+  type        = string
+  default     = null
+}
+
+variable "linux_vm_ultra_disks_enabled" {
+  description = "Enable Ultra SSD capability"
+  type        = bool
+  default     = false
+}
+
+variable "linux_vm_boot_diag_uri" {
+  description = "Storage Account URI for Boot Diagnostics"
+  type        = string
+  default     = null
+}
+
+variable "linux_vm_identity_type" {
+  description = "Type of Managed Service Identity"
+  type        = string
+  default     = null
+}
+
+variable "linux_vm_identity_ids" {
+  description = "User Assigned Managed Identity IDs"
+  type        = list(string)
+  default     = []
+}
+
+variable "linux_vm_custom_tags" {
+  description = "Custom tags for VM"
+  type        = map(string)
+  default     = {}
+}
+
