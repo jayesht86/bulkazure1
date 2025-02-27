a455e8e811c0db526bcfb8c9beffb21efc12cc2d
jayesht86 <37490933+jayesht86@users.noreply.github.com>
2025-02-27 00:12:21 -0800

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,1 +1,14 @@
-
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {
+    #skip_provider_registration = true
+  }
+  subscription_id = "59bab8f2-1319-472b-bb69-314d1b034ec0"
+}
